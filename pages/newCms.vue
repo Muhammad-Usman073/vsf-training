5431f42d86db0022822132ed57411e8e1bcf79a5
Muhammad-Usman073 <muhammadusman073@outlook.com>
2024-02-15 16:25:38 +0500

    changes in cms

--- pages/newCms.vue
+++ pages/newCms.vue
@@ -1,50 +1,51 @@
 <template>
   <div>
     <div v-if="isAuthenticated">
-      <content-block :content=" userBlock " title="user block"/>
+      <content-block
+        :content=" userBlock "
+        title="user block"
+      />
     </div>
     <div v-else>
-      <content-block :content=" guestBlock " title="guest block"/>
+      <content-block
+        :content=" guestBlock "
+        title="guest block"
+      />
     </div>
   </div>
 </template>
+
 <script>
-import {onMounted, ref,useContext,getCurrentInstance} from '@nuxtjs/composition-api';
+import { onMounted, ref } from '@nuxtjs/composition-api';
 import ContentBlock from '~/components/ContentBlock.vue';
-import ContentBlocks from '~/components/ContentBlocks.vue';
-import {useContent, useUser} from '~/composables';
-import userGetters from '~/modules/customer/getters/userGetters';
+import { useContent, useUser } from '~/composables';
 
 export default {
   components: {
     ContentBlock,
-    ContentBlocks
   },
   setup() {
-    const userBlock = ref(null)
-    const guestBlock = ref(null)
-    const {loadBlocks} = useContent();
+    const userBlock = ref(null);
+    const guestBlock = ref(null);
+    const { loadBlocks } = useContent();
     const {
       isAuthenticated,
-      load
-    } = useUser()
+      load,
+    } = useUser();
     onMounted(async () => {
       await load();
-      const newBlock = await loadBlocks({identifiers: ['my-block', 'guest-block']})
-      const blockValue = newBlock.map(items => items)
-      userBlock.value = blockValue?.[0].content
-      guestBlock.value = blockValue?.[1].content
-    })
-    console.log(useContext(),"context")
-    console.log(getCurrentInstance(),"current instance")
+      const newBlock = await loadBlocks({ identifiers: ['my-block', 'guest-block'] });
+      const blockValue = newBlock.map((items) => items);
+      userBlock.value = blockValue?.[0].content;
+      guestBlock.value = blockValue?.[1].content;
+    });
     return {
       ContentBlock,
-      ContentBlocks,
       isAuthenticated,
       userBlock,
-      guestBlock
-    }
-  }
+      guestBlock,
+    };
+  },
 
-}
+};
 </script>
